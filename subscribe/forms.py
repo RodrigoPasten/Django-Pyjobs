7d1dd2339329714dcd0d168094035f253c65dd40
rpasten <rodrigo.pasten.c@gmail.com>
2023-07-05 15:30:51 -0400

    Se agrega validadores a forms

--- subscribe/forms.py
+++ subscribe/forms.py
@@ -1,7 +1,19 @@
 from django import forms
 
 
+def validate_comma(value):
+    if "," in value:
+        raise forms.ValidationError('El nombre no puede tener coma')
+    return value
+
+
 class SubscribeForm(forms.Form):
-    first_name = forms.CharField(max_length=100)
+    first_name = forms.CharField(max_length=100, label='Nombre', validators=[validate_comma])
     last_name = forms.CharField(max_length=100)
-    email = forms.EmailField(max_length=100)
+    email = forms.EmailField(
+        max_length=100,
+        error_messages={
+            'required': 'Por favor ingresa tu correo electrónico',
+            'invalid': 'El correo proporcionado no es válido',
+        },
+        required=True)
