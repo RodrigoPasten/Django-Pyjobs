d4b2d02372a96f452c5e3b3cedec95e4ad570f5c
rpasten <rodrigo.pasten.c@gmail.com>
2023-07-05 21:38:43 -0400

    Se agrega archivo CSS

--- subscribe/models.py
+++ subscribe/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
+# declaramos la constante contenedora de las opciones
+NEWLETTER_OPTION = [
+    ("W", "Weekly"),
+    ("M", "Monthly")
+]
+
 
 class Subscribe(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     email = models.EmailField(max_length=100)
+    option = models.CharField(max_length=2, choices=NEWLETTER_OPTION, default="W")
 
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
