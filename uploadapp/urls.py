56bebecd478e0ebe93b4814962a2515dbf9ed1f4
rpasten <rodrigo.pasten.c@gmail.com>
2023-07-06 20:09:12 -0400

    Agrega estilos definitivos

--- uploadapp/urls.py
+++ uploadapp/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('image/', views.upload_image, name="upload_image"),
-
+    path('file/', views.upload_file, name="upload_file"),
 ]
--- uploadapp/views.py
+++ uploadapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from uploadapp.forms import UploadForm
+from uploadapp.forms import UploadForm, UploadFileForm
 
 
 def upload_image(request):
@@ -13,3 +13,16 @@ def upload_image(request):
         form = UploadForm()
     context = {'form': form}
     return render(request, 'uploadapp/add_image.html', context)
+
+
+def upload_file(request):
+    if request.POST:
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            saved_object = form.instance
+            return render(request, 'uploadapp/add_file.html', {"form": form, "saved_object": saved_object})
+    else:
+        form = UploadFileForm()
+    context = {'form': form}
+    return render(request, 'uploadapp/add_file.html', context)
